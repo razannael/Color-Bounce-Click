92650f6301365101e5096139d51c364bf25096b7
razannael <nailrazan40@gmail.com>
2024-05-10 16:22:15 +0300

    Multi Colored Balls

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import * as CANNON from 'cannon-es';
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.shadowMap.enabled = true;
+
 document.body.appendChild(renderer.domElement);
 
 // Sets the color of the background
@@ -20,18 +24,37 @@ const camera = new THREE.PerspectiveCamera(
 const orbit = new OrbitControls(camera, renderer.domElement);
 
 // Camera positioning
-camera.position.set(6, 8, 14);
+camera.position.set(0, 2, 7);
 orbit.update();
 
+const world = new CANNON.World({gravity: new CANNON.Vec3(0, -9.82, 0)});
+const planeGeo = new THREE.PlaneGeometry(10, 10);
+const planeMat = new THREE.MeshStandardMaterial({
+    color: 'green',
+    side: THREE.DoubleSide
+});
+const planeMesh = new THREE.Mesh(planeGeo, planeMat);
+scene.add(planeMesh);
+planeMesh.receiveShadow = true;
 
 const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
 scene.add(directionalLight);
-directionalLight.position.set(0, 50, 0);
+directionalLight.position.set(0, 10, 20);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
 
-
+const planePhysMat = new CANNON.Material();
+const planeBody = new CANNON.Body({
+    type : CANNON.Body.STATIC,
+    shape: new CANNON.Box(new CANNON.Vec3(5, 5, 0.001)),
+    material: planePhysMat
+});
+planeBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+world.addBody(planeBody);
 
 const mouse = new THREE.Vector2();
 const intersectionPoint = new THREE.Vector3();
@@ -48,19 +71,51 @@ window.addEventListener('mousemove', function(event) {
     raycaster.ray.intersectPlane(plane, intersectionPoint);
 });
 
+const meshes = [];
+const bodies = [];
+
 window.addEventListener('click', function(event) {
     const geometry = new THREE.SphereGeometry(0.2, 32, 32);
     const material = new THREE.MeshStandardMaterial({
-        color: 'pink',
+        color: Math.random() * 0xffffff,
         roughness: 0,
         metalness: 0
     });
     const sphere = new THREE.Mesh(geometry, material);
-    sphere.position.copy(intersectionPoint);
+   // sphere.position.copy(intersectionPoint);
     scene.add(sphere);
+    sphere.castShadow = true;
+    
+
+    const spherePhysMat = new CANNON.Material();
+    const sphereBody = new CANNON.Body({
+        mass: 0.3,
+        shape: new CANNON.Sphere(0.2),
+        position : new CANNON.Vec3(intersectionPoint.x, intersectionPoint.y, intersectionPoint.z),
+        material: spherePhysMat
+    });
+    world.addBody(sphereBody);
+
+    const planeSphereContactMaterial = new CANNON.ContactMaterial(planePhysMat, spherePhysMat, {
+        restitution: 0.3
+    });
+    world.addContactMaterial(planeSphereContactMaterial);
+
+    meshes.push(sphere);
+    bodies.push(sphereBody);
 })
 
+const timeStep = 1 / 60;
 function animate() {
+    world.step(timeStep);
+
+    planeMesh.position.copy(planeBody.position);
+    planeMesh.quaternion.copy(planeBody.quaternion);
+
+    for (let i = 0; i < meshes.length; i++) {
+        meshes[i].position.copy(bodies[i].position);
+        meshes[i].quaternion.copy(bodies[i].quaternion);
+    }
     renderer.render(scene, camera);
 }
 
